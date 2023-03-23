8c4e022ac0f221085eadacbf2c94aea72d3f8e3a
CyntTech <101424204+CyntTech@users.noreply.github.com>
2023-03-23 07:40:36 +0100

    stdin display of user input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,9 +3,8 @@ process.stdin.setEncoding('utf8');
 process.stdout.write('welcome to Holberton School, what is your name?\n ');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  if (input !== null) {
-    process.stdout.write(`Your name is: ${input}`);
-  }
+
+  if (input) process.stdout.write(`Your name is: ${input}`);
 });
 
 process.stdin.on('end', () => {
